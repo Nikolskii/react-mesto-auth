31629bf58d839b2f072fdb7d06d8ff74c26c7cd7
Denis Nikolskiy <den-apple@ya.ru>
2022-11-01 21:03:35 +0200

    feat: добавить стили для InfoTooltip

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,12 +1,22 @@
+import regSuccessImg from '../images/registration-success.svg';
+import regUnsuccessImg from '../images/registration-unsuccess.svg';
+
 function InfoTooltip({ isOpen, isRegistrationSuccess }) {
   return (
     <section
       className={`popup ${isOpen && 'popup_opened'}`}
-      // className={`popup popup_opened'`}
-      aria-label="ЗАМЕНИТЬ"
+      aria-label="Результат регистрации"
     >
-      <div className="popup__container">
-        {/* <img src={isRegistrationSuccess ? ''} */}
+      <div className="popup__container popup__container_type_reg">
+        <img
+          className="popup__reg-image"
+          src={isRegistrationSuccess ? regSuccessImg : regUnsuccessImg}
+        />
+        <h2 className="popup__title popup__title_place_reg">
+          {isRegistrationSuccess
+            ? 'Вы успешно зарегистрировались!'
+            : 'Что-то пошло не так! Попробуйте ещё раз.'}
+        </h2>
       </div>
     </section>
   );
