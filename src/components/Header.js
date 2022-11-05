285e0c5a9238165167d9a840299c546019906c67
Denis Nikolskiy <den-apple@ya.ru>
2022-11-05 10:30:29 +0200

    chore: актуализировать README.md

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,44 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import logo from '../images/header-logo.svg';
 
 function Header({ loggedIn, onSignout, email }) {
-  // console.log(email);
-  const location = useLocation();
-
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Логотип проекта Место" />
 
-      {loggedIn ? (
-        <div className="header__auth">
-          <p className="header__email">{email}</p>
-          <button onClick={onSignout} className="header__button">
-            Выйти
-          </button>
-        </div>
-      ) : (
-        <Link
-          className="header__link"
-          to={location.pathname === '/sign-in' ? 'sign-up' : 'sign-in'}
-        >
-          {location.pathname === '/sign-in' ? 'Регистрация' : 'Войти'}
-        </Link>
-      )}
+      <Routes>
+        <Route
+          exact
+          path="sign-in"
+          element={
+            <Link className="header__link" to="/sign-up">
+              Регистрация
+            </Link>
+          }
+        />
+
+        <Route
+          exact
+          path="sign-up"
+          element={
+            <Link className="header__link" to="/sign-in">
+              Войти
+            </Link>
+          }
+        />
+
+        <Route
+          path="/"
+          element={
+            <div className="header__auth">
+              <p className="header__email">{email}</p>
+              <button onClick={onSignout} className="header__button">
+                Выйти
+              </button>
+            </div>
+          }
+        />
+      </Routes>
     </header>
   );
 }
