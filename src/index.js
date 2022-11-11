9acb1776818af4e059c7b5cce00e4ca19f64fb0f
Denis Nikolskiy <den-apple@ya.ru>
2022-11-11 18:19:42 +0200

    Deploy GH-Pages

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/react-mesto-auth">
     <App />
   </BrowserRouter>
 );
