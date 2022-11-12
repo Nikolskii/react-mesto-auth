a8d08ae2b7a17da5c1fea8b8107884f499d3f7d7
Denis Nikolskiy <den-apple@ya.ru>
2022-11-12 11:55:49 +0200

    chore: formik vadilation Register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,61 +1,72 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 function Register({ onRegister }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  function handleSubmit(evt) {
-    evt.preventDefault();
+  // function handleSubmit(evt) {
+  //   evt.preventDefault();
 
-    onRegister({ email, password });
-  }
+  //   onRegister({ email, password });
+  // }
 
   return (
     <section className="auth">
       <h1 className="auth__title">Регистрация</h1>
 
-      <form className="auth-form" onSubmit={handleSubmit} name="register">
-        <fieldset className="auth-form__fieldset">
-          <input
-            onChange={(evt) => {
-              setEmail(evt.target.value);
-            }}
-            value={email}
-            className="auth-form__input"
-            type="text"
-            name="email"
-            id="email"
-            placeholder="Email"
-            minLength="5"
-            maxLength="30"
-            required
-          />
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .email('Invalid email address')
+            .required('Required'),
+          password: Yup.string()
+            .min(4, 'Must be 4 characters or more')
+            .required('Required'),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form className="auth-form">
+          <fieldset className="auth-form__fieldset">
+            <Field
+              name="email"
+              type="text"
+              placeholder="Email"
+              className="auth-form__input"
+            />
+            <span style={{ color: 'red' }}>
+              <ErrorMessage name="email" />
+            </span>
 
-          <input
-            onChange={(evt) => {
-              setPassword(evt.target.value);
-            }}
-            value={password}
-            className="auth-form__input"
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Пароль"
-            minLength="4"
-            maxLength="30"
-            required
-          />
-        </fieldset>
+            <Field
+              name="password"
+              type="text"
+              placeholder="password"
+              className="auth-form__input"
+            />
+            <ErrorMessage name="password" />
+          </fieldset>
 
-        <button
-          className="auth-form__button"
-          type="submit"
-          value={'Зарегистрироваться'}
-        >
-          Зарегистрироваться
-        </button>
-      </form>
+          <button
+            className="auth-form__button"
+            type="submit"
+            // value={'Зарегистрироваться'}
+          >
+            Зарегистрироваться
+          </button>
+        </Form>
+      </Formik>
 
       <p className="auth__text">
         Уже зарегистрированы?&nbsp;
